6c550fdfa1ae2ec39936105c75900024adc1a68d
MartinPlascencia <martin.plascencia@live.com.mx>
2026-02-26 19:46:03 -0600

    Agregado Powerup Manager.

--- Assets/Scripts/PlatformsManager.cs
+++ Assets/Scripts/PlatformsManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlatformsManager : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class PlatformsManager : MonoBehaviour
     private int initialPlatforms = 5;
     [SerializeField]
     private float speed = 5f;
+    [SerializeField]
+    private UnityEvent<Platform> onPlatformPassed;
     private bool isRunning = true;
     private GameObject lastPlatform;
     private int platformsInstantiated = 0;
@@ -58,6 +61,7 @@ public class PlatformsManager : MonoBehaviour
             newPlatform.transform.SetParent(transform);
             newPlatform.transform.localPosition = spawnPosition + newPlatform.GetComponent<Collider>().bounds.size.z * Vector3.forward * 0.5f;
             lastPlatform = newPlatform;
+            onPlatformPassed?.Invoke(newPlatform.GetComponent<Platform>());
         }
     }
     private void Update()
